9010f9239528b2d5f913e9b2197aaac7e373801f
royce-reed <rjreed1@gmail.com>
2022-08-11 09:15:42 -0500

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Listening on port http://localhost:${PORT}`);
+});
